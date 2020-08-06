6b7122d72d9b1e1f2589d4b1088fd9def3951fbb
LeonardoFerrari <l201164@dac.unicamp.br>
2020-08-06 20:06:16 -0300

    Adição do 'Link' do react-router-dom para aplicação do conceito SPA

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import logoImg from '../../assets/images/logo.svg'
+import { Link } from 'react-router-dom';
 
 import landingImg from '../../assets/images/landing.svg'
 import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+
+import {    } from 'react-router-dom'
 
 import './styles.css';
 
@@ -22,15 +25,15 @@ function Landing(){
                 className="hero-image"
                 />
             <div className="buttons-container">
-                <a href="" className="study">
+                <Link to="/study" className="study">
                     <img src={studyIcon} alt="Estudar"/>
                     Estudar
-                </a>
+                </Link>
 
-                <a href="" className="give-classes">
+                <Link to="/give-classes" className="give-classes">
                     <img src={giveClassesIcon} alt="Dar aula"/>
                     Dar aula
-                </a>
+                </Link>
                 </div>
                 <span className="total-connetions">Total de 1000 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/></span>
             </div>
